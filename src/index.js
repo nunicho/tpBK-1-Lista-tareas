ec1ff6baa13eae69b72aea237fccd7ccc8da978c
Mauricio-Alonso <conta.alonso@gmail.com>
2022-10-26 14:11:41 -0300

    Se agregaron los middelwares.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import  express  from 'express';
-
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
 // crear una isntancia de express
 
 const app = express();
@@ -12,4 +14,19 @@ app.listen(app.get('port'),()=>{
     console.log('Estoy en el puerto '+ app.get('port'));
 })
 
-console.log("Hola mundo")
+// middleware: funciones que se ejecutan antes de las rutas
+// da información extra en la terminal
+app.use(morgan('dev'));
+//permitir peticiones remotas
+app.use(cors()); 
+// middleware para interpretar los objetos JSON
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+// cargar un archivo estático
+app.use(express.static(path.join(__dirname,'../public')));
+
+//rutas: nombre de dominio +----
+//http://localhost:4000/prueba
+app.get('/productos', (req, res)=>{
+    res.send('Esto es una prueba de la petición GET')
+})
